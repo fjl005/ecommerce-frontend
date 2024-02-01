cb97420c37af7be85dc3e88e9a222c9de953cce8
fjl005 <frank.jk.lee96@gmail.com>
2024-01-31 20:21:02 -0800

    testing subdomain

--- src/components/miscellaneous/axios.js
+++ src/components/miscellaneous/axios.js
@@ -2,14 +2,16 @@ import axios from 'axios';
 
 // Axios Configuration. Need Credentials to send cookies.
 export const axiosWithAuth = axios.create({
-    baseURL: 'https://fetsy-ecommerce-backend-2.onrender.com/',
+    baseURL: 'https://api.fetsyshoponline.com/',
+    // baseURL: 'https://fetsy-ecommerce-backend-2.onrender.com/',
     // baseURL: 'http://localhost:10000/',
     // baseURL: 'http://127.0.0.1:10000/',
     withCredentials: true,
 });
 
 export const axiosNoAuth = axios.create({
-    baseURL: 'https://fetsy-ecommerce-backend-2.onrender.com/',
+    baseURL: 'https://api.fetsyshoponline.com/',
+    // baseURL: 'https://fetsy-ecommerce-backend-2.onrender.com/',
     // baseURL: 'http://localhost:10000/',
     // baseURL: 'http://127.0.0.1:10000/',
 
