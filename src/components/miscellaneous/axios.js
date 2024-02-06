182767e9735b39fdbb59278989cae9c4f1207604
fjl005 <frank.jk.lee96@gmail.com>
2024-02-05 21:09:58 -0800

    pushing the npm run build

--- src/components/miscellaneous/axios.js
+++ src/components/miscellaneous/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const axiosWithAuth = axios.create({
-    // baseURL: 'https://api.fetsyshoponline.com/',
-    baseURL: 'http://localhost:10000/',
+    baseURL: 'https://api.fetsyshoponline.com/',
+    // baseURL: 'http://localhost:10000/',
     withCredentials: true,
 });
 
 export const axiosNoAuth = axios.create({
-    // baseURL: 'https://api.fetsyshoponline.com/',
-    baseURL: 'http://localhost:10000/',
+    baseURL: 'https://api.fetsyshoponline.com/',
+    // baseURL: 'http://localhost:10000/',
 });
